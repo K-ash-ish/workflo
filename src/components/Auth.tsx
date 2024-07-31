6640c83bcaf934ef019e196139bbfc9698e08622
K-ash-ish <kashishsondhiya@gmail.com>
2024-08-01 03:29:19 +0530

    asdf

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -14,12 +14,6 @@ function Auth({ authType }: { authType: string }) {
     {} as Credentials
   );
 
-  if (success) {
-    console.log("redirecting from auth compe");
-    setCredentials({} as Credentials);
-    // redirect("/dashboard");
-    // router.push("/dashboard");
-  }
   function handleInput(inputValue: string, valueToUpdate: string) {
     setCredentials((prevValue) => {
       return { ...prevValue, [valueToUpdate]: inputValue };
@@ -37,6 +31,7 @@ function Auth({ authType }: { authType: string }) {
       signup(credentials.email, credentials.password, credentials.text);
       console.log("signup");
     }
+    setCredentials({} as Credentials);
   }
   return (
     <div className="md:w-[480px] w-5/6 min-h-[360px] bg-white rounded-md border-2 flex flex-col justify-center gap-6 p-10 ">
