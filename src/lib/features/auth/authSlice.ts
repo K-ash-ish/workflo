db206a42b3a117ce28cfe25b9c9716900f3bb0a9
K-ash-ish <kashishsondhiya@gmail.com>
2024-10-07 18:20:57 +0530

    refactoring authSlice using addMatcher instead of addCase

--- src/lib/features/auth/authSlice.ts
+++ src/lib/features/auth/authSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, isAnyOf } from "@reduxjs/toolkit";
 import { login, logout, signup, verifyToken } from "./authActions";
 import { RootState } from "@/lib/store";
 
@@ -21,58 +21,32 @@ export const authSlice = createSlice({
   name: "users",
   reducers: {},
   extraReducers: (builder) => {
-    builder.addCase(login.pending, (state) => {
-      state.status = "loading";
+    builder.addCase(logout.fulfilled, () => {
+      return InitialState;
     });
-    builder.addCase(login.fulfilled, (state, action) => {
-      state.status = "idle";
-      if (action.payload) {
-        state.userName = action.payload.data.name;
-        state.isLoggedIn = true;
+    builder.addMatcher(
+      isAnyOf(login.pending, signup.pending, logout.pending),
+      (state) => {
+        state.status = "loading";
       }
-    });
-    builder.addCase(login.rejected, (state, action) => {
-      state.status = "failed";
-      console.log("error from authslice: ", action.payload);
-    });
-    builder.addCase(verifyToken.fulfilled, (state, action) => {
-      state.status = "idle";
-      if (action.payload) {
-        state.userName = action.payload.data.name;
-        state.isLoggedIn = true;
+    );
+    builder.addMatcher(
+      isAnyOf(login.fulfilled, signup.fulfilled, verifyToken.fulfilled),
+      (state, action) => {
+        state.status = "idle";
+        if (action.payload) {
+          state.userName = action.payload.data.name;
+          state.isLoggedIn = true;
+        }
       }
-    });
-    builder.addCase(verifyToken.rejected, (state, action) => {
-      state.status = "failed";
-      console.log("error authenticating");
-    });
-    builder.addCase(signup.pending, (state, action) => {
-      state.status = "loading";
-    });
-    builder.addCase(signup.fulfilled, (state, action) => {
-      state.status = "idle";
-      if (action.payload) {
-        state.userName = action.payload.data.name;
-        state.isLoggedIn = true;
+    );
+    builder.addMatcher(
+      isAnyOf(login.rejected, signup.rejected, logout.rejected),
+      (state, action) => {
+        state.status = "failed";
+        console.log("error from authslice: ", action.payload);
       }
-    });
-    builder.addCase(signup.rejected, (state, action) => {
-      state.status = "failed";
-      console.log("error signing up");
-    });
-    builder.addCase(logout.pending, (state, action) => {
-      state.status = "loading";
-    });
-    builder.addCase(logout.fulfilled, (state) => {
-      state.status = "idle";
-      state.isLoggedIn = false;
-      state.userName = "";
-      state.userPicture = "";
-    });
-    builder.addCase(logout.rejected, (state, action) => {
-      state.status = "failed";
-      console.log("error logging out");
-    });
+    );
   },
 });
 
