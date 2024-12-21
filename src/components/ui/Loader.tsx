111855f1f982a755b7e005ec95415bcb2c397aa0
K-ash-ish <kashishsondhiya@gmail.com>
2024-12-21 22:42:56 +0530

    update TaskBoardLoader colors for improved visual distinction

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -9,10 +9,10 @@ export function ProfilePicLoader() {
 export function TaskBoardLoader() {
   return (
     <div className="bg-white w-full min-h-[200px] py-2 px-4 rounded-md flex gap-4 animate-pulse ">
-      <div className="w-1/4 h-24 bg-gray-200 rounded-md"> </div>
-      <div className="w-1/4 h-24 bg-gray-200 rounded-md"> </div>
-      <div className="w-1/4 h-24 bg-gray-200 rounded-md"> </div>
-      <div className="w-1/4 h-24 bg-gray-200 rounded-md"> </div>
+      <div className="w-1/4 h-24 bg-red-100 rounded-md"> </div>
+      <div className="w-1/4 h-24 bg-yellow-100 rounded-md"> </div>
+      <div className="w-1/4 h-24 bg-cyan-100 rounded-md"> </div>
+      <div className="w-1/4 h-24 bg-green-100 rounded-md"> </div>
     </div>
   );
 }
