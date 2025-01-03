0d79b74577c919713e3fad47c6f2910637ab58a6
K-ash-ish <kashishsondhiya@gmail.com>
2025-01-03 23:30:21 +0530

    add All Tasks and Team pages with basic structure

---
+++ src/app/dashboard/alltasks/page.tsx
@@ -0,0 +1,8 @@
+function page() {
+  return (
+    <div>
+      <h1>All tasks</h1>
+    </div>
+  );
+}
+export default page;
---
+++ src/app/dashboard/team/page.tsx
@@ -0,0 +1,8 @@
+function page() {
+  return (
+    <div>
+      <h1>Team</h1>
+    </div>
+  );
+}
+export default page;
