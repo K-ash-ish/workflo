831dfbcbd8aed83470fa3da9737786a0a0c3f2f7
K-ash-ish <kashishsondhiya@gmail.com>
2025-01-10 18:06:54 +0530

    refactor dashboard layout to optimize task statistics rendering and improve user name display logic

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Sidenav from "@/components/Sidenav";
 import { ModalProvider } from "@/context/ModalContext";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useAppDispatch, useAppSelector } from "@/lib/hooks";
 import { verifyToken } from "@/lib/features/auth/authActions";
 import { Search } from "lucide-react";
@@ -12,6 +12,43 @@ import { quickStatsEl } from "@/constant";
 import TaskModal from "@/components/ui/TaskModal";
 import Sheet from "@/components/ui/Sheet";
 
+function TaskStats() {
+  const {
+    totalTasksFinished,
+    totalTasksInProgress,
+    totalTasksUnderReview,
+    totalTasks,
+    totalOverDueTasks,
+  } = filterTasks();
+
+  return quickStatsEl?.map(({ title, color }, i) => {
+    let total = 0;
+    if (title === "total tasks") {
+      total = totalTasks;
+    } else if (title === "in progress") {
+      total = totalTasksInProgress;
+    } else if (title === "completed") {
+      total = totalTasksFinished;
+    } else if (title === "overdue") {
+      total = totalOverDueTasks;
+    } else if (title === "under review") {
+      total = totalTasksUnderReview;
+    }
+    return (
+      <div
+        key={i + title}
+        className={`${color}   p-1  flex-1 md:px-2 md:py-3  rounded-md`}
+      >
+        <div className="flex flex-col md:gap-2 justify-between h-full  ">
+          <h4 className=" text-[10px] md:text-base ">{title}</h4>
+          <p className="text-gray-400 text-sm md:text-lg">{total}</p>
+        </div>
+        <div className="hidden md:block"></div>
+      </div>
+    );
+  });
+}
+
 export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
@@ -19,7 +56,7 @@ export default function Layout({
   const dispatch = useAppDispatch();
   const user = useAppSelector(selectUser);
   const status = useAppSelector((state) => state.auth.status);
-  const userName = () => {
+  const userName = useMemo(() => {
     return (
       <div className="font-barlow font-semibold  text-xl  capitalize flex items-center md:gap-2 text-center ">
         Welcome Back,
@@ -31,45 +68,11 @@ export default function Layout({
         !
       </div>
     );
-  };
-  const taskStats = () => {
-    return quickStatsEl?.map(({ title, color }, i) => {
-      const {
-        totalTasksFinished,
-        totalTasksInProgress,
-        totalTasksUnderReview,
-        totalTasks,
-        totalOverDueTasks,
-      } = filterTasks();
-      let total = 0;
-      if (title === "total tasks") {
-        total = totalTasks;
-      } else if (title === "in progress") {
-        total = totalTasksInProgress;
-      } else if (title === "completed") {
-        total = totalTasksFinished;
-      } else if (title === "overdue") {
-        total = totalOverDueTasks;
-      } else if (title === "under review") {
-        total = totalTasksUnderReview;
-      }
-      return (
-        <div
-          key={i + title}
-          className={`${color}   p-1  flex-1 md:px-2 md:py-3  rounded-md`}
-        >
-          <div className="flex flex-col md:gap-2 justify-between h-full  ">
-            <h4 className=" text-[10px] md:text-base ">{title}</h4>
-            <p className="text-gray-400 text-sm md:text-lg">{total}</p>
-          </div>
-          <div className="hidden md:block"></div>
-        </div>
-      );
-    });
-  };
+  }, [status, user.userName]);
+
   useEffect(() => {
     dispatch(verifyToken());
-  }, []);
+  }, [dispatch]);
 
   return (
     <ModalProvider>
@@ -94,10 +97,10 @@ export default function Layout({
                 className="w-full py-2 px-3 bg-white rounded-md    focus:border-none focus:outline-[#d2d2d2] border border-gray-4 00  text-[#797979] placeholder-gray-400 placeholder:pl-1  caret-gray-400 "
               />
             </label>
-            {userName()}
+            {userName}
           </div>
           <div className=" flex gap-1 md:gap-2 font-semibold capitalize px-2 ">
-            {taskStats()}
+            <TaskStats />
           </div>
           {children}
         </div>
