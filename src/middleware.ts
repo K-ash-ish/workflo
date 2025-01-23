6204b96e8f8bfa2e31788b7604966b2a815567b9
K-ash-ish <kashishsondhiya@gmail.com>
2025-01-23 22:26:50 +0530

    reset to prev commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,37 +11,23 @@ async function isAuthenticated(req: NextRequest) {
   if (!isVerified) {
     return false;
   }
-  return isVerified;
+  return true;
 }
 
 export async function middleware(request: NextRequest, response: NextResponse) {
   const url = request.url;
   const isAuth = await isAuthenticated(request);
-
   if (!isAuth) {
     if (url.includes("login") || url.includes("signup")) {
-      if (url.includes("verify-otp"))
-        return NextResponse.redirect(new URL("/login", request.url));
       return NextResponse.next();
     }
 
     return NextResponse.redirect(new URL("/login", request.url));
   }
-  const active = isAuth.payload.active;
-  if (!active) {
-    if (url.includes("verify-otp")) {
-      return NextResponse.next();
-    }
-    return NextResponse.redirect(new URL("/user/verify-otp", request.url));
+  if (url.includes("login") || url.includes("signup")) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
   }
 
-  if (
-    url.includes("login") ||
-    url.includes("signup") ||
-    url.includes("verify-otp")
-  ) {
-    return NextResponse.redirect(new URL("/", request.url));
-  }
   return NextResponse.next();
 }
 export const config = {
